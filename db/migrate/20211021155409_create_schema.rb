b7c6a0ba112afb59ffd9538c23e1f5d31a0b6e69
Brad Trick <76491823+brtrick@users.noreply.github.com>
2022-10-21 14:17:52 -0400

    Include standard Rails tables in schema

--- db/migrate/20211021155409_create_schema.rb
+++ db/migrate/20211021155409_create_schema.rb
@@ -1,5 +1,8 @@
 class CreateSchema < ActiveRecord::Migration[7.0]
   def change
     ApplicationRecord.connection.create_schema('authenticate_me')
+    ActiveRecord::Base.table_name_prefix = 'authenticate_me.'
+    ActiveRecord::SchemaMigration.create_table
+    ActiveRecord::InternalMetadata.create_table
   end
 end
