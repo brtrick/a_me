0dd62c4f64d284bee266047fe8d9d4884c4897e2
Brad Trick <76491823+brtrick@users.noreply.github.com>
2022-10-24 12:45:20 -0400

    Update render.yaml to include SCHEMA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-ENV['SCHEMA'] = "authenticate_me"
+ENV['SCHEMA'] = "authenticate_me_jon"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
